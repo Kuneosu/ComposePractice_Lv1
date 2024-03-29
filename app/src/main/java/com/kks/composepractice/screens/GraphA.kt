2ae76a2078de2ba6bdcdcba76d244a4249f2bc1f
Kuneosu <kks92155784@gmail.com>
2024-03-29 10:31:16 +0900

    Add button to Graph A of Graph Practice

--- app/src/main/java/com/kks/composepractice/screens/GraphA.kt
+++ app/src/main/java/com/kks/composepractice/screens/GraphA.kt
@@ -1,15 +1,28 @@
 package com.kks.composepractice.screens
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,53 +38,84 @@ fun GraphA() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        GraphAGauge(percent = 70f, maxPercent = 100f)
+        GraphAGauge()
     }
 }
 
 @Composable
-fun GraphAGauge(percent: Float, maxPercent: Float) {
+fun GraphAGauge() {
+    var percent by remember { mutableFloatStateOf(0.0f) }
+    val maxPercent = 100.0f
     val colorPercent = (percent / maxPercent) * 360
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp)
-            .aspectRatio(1f)
-    ) {
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            drawArc(
-                color = Color.LightGray,
-                startAngle = 0f,
-                sweepAngle = 360f,
-                useCenter = false,
-                style = Stroke(35f)
-            )
-
-            drawArc(
-                color = Color.Blue,
-                startAngle = -90f,
-                sweepAngle = colorPercent,
-                useCenter = false,
-                style = Stroke(35f)
-            )
-        }
-
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+                .aspectRatio(1f)
         ) {
-            Text(
-                text = "${((percent / maxPercent) * 100).toInt()} %",
-                fontSize = 35.sp
-            )
-            Text(
-                text = "$percent / $maxPercent",
-                fontSize = 20.sp
-            )
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                drawArc(
+                    color = Color.LightGray,
+                    startAngle = 0f,
+                    sweepAngle = 360f,
+                    useCenter = false,
+                    style = Stroke(35f)
+                )
+
+                drawArc(
+                    color = Color.Blue,
+                    startAngle = -90f,
+                    sweepAngle = colorPercent,
+                    useCenter = false,
+                    style = Stroke(35f)
+                )
+            }
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "${((percent / maxPercent) * 100).toInt()} %",
+                    fontSize = 35.sp
+                )
+                Text(
+                    text = "$percent / $maxPercent",
+                    fontSize = 20.sp
+                )
+            }
+        }
+        Spacer(modifier = Modifier.size(30.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            IconButton(onClick = {
+                if (percent > 0) percent -= 10.0f
+            }) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier.size(50.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.size(50.dp))
+
+            IconButton(onClick = {
+                if (percent < maxPercent) percent += 10.0f
+            }) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowUp,
+                    contentDescription = "Up Arrow",
+                    modifier = Modifier.size(50.dp)
+                )
+            }
         }
     }
+
 }
 
 @Composable
